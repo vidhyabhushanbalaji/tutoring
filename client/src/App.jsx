a143efee72e553e1b23d8c2e96dbb5ebc1654ac9
Vidhyabhushan Balaji <vbalaji06@outlook.com>
2026-06-27 01:05:36 +0100

    it works!!!!!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,12 @@ import Setup from './Setup';
 import axios from 'axios'
 
 function App() {
+
+  /*
   const [count, setCount] = useState(0)
   
   const [email, setEmail] = useState('')
   const [pwd, setPwd] = useState('')
-
-  const [setup, setSetup] = useState(false)
-
-  function maybeDetails(){
-    if (setup){
-      return <Setup />
-    }
-    else{
-      return (<p>Nothing</p>)
-    }
-  }
-
 
   function handleSubmit(event){
     event.preventDefault();
@@ -42,51 +32,20 @@ function App() {
       }
     );
   }
+  */
 
   return (
     <>
-      <section id="top">
-        <div>  
-          <h1>Welcome</h1>
-          <p>
-            Login below
-          </p>
-        </div>
 
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-        
-      </section>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/setup" element={<Setup/>}/>
+          <Route exact path="/" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
 
-      <section id="next-steps">
-        <div id="login">
-          <form onSubmit={handleSubmit}>
-            <input name ="email"
-            value = {email}
-            onChange = {e => setEmail(e.target.value)}></input> 
-            <br></br>
-            <input name ="pwd" 
-            value = {pwd}
-            type="password"
-            onChange = {e => setPwd(e.target.value)}
-            ></input> 
-            
-            <br></br>
-
-            <button>Login</button>
-
-          </form>
-
-          {maybeDetails()}
-        
-        </div>
-        
-      </section>
     </>
   )
 }
